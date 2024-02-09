a0d5cb5465f00ca4b79e2f1df243ec0063417912
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-09 10:33:28 -0600

    Updated project name for WandB to use argument

--- hw0.py
+++ hw0.py
@@ -81,7 +81,7 @@ def execute_exp(args):
 
 
 	# Initialize WANDB
-	wandb.init(project='XOR', name='xor_%d'%(args.exp), notes=argstring, config=vars(args))
+	wandb.init(project=args.project, name='xor_%d'%(args.exp), notes=argstring, config=vars(args))
 
 	# Load training set
 	fp = open("hw0_dataset.pkl", "rb")
