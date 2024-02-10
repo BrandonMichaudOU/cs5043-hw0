fadfea6fe90ffe6e8b879d195fef5f628d02bb6a
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-10 12:00:15 -0600

    commit

--- batch.sh
+++ batch.sh
@@ -21,7 +21,7 @@
 conda activate tf
 
 # Change this line to start an instance of your experiment
-python hw0.py --project 'hw0' --lrate 0.005 --nonlinearity 'tanh' --nonlinearity_output 'tanh' --hidden 9 5 --epochs 1000 --exp 6 -vv
+python hw0.py --project 'hw0' --lrate 0.005 --nonlinearity 'tanh' --nonlinearity_output 'tanh' --hidden 9 7 --epochs 1000 --exp 6 -vv
 
 
 # parser.add_argument('--lrate', type=float, default=0.001, help='learning rate')
